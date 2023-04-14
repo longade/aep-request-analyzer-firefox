a4a2dcb1f227a36f5418ded0e8aabb82a1443c04
longade <davide.longo97@gmail.com>
2023-04-14 21:56:50 +0200

    feat: added permission button to make addon work

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,16 @@
 var enabled = true; //enabled by default
 const enableToggle = document.querySelector('#enable-toggle');
 const enableToggleLabel = document.querySelector('.toggle-label');
+const buttonDiv = document.querySelector('#button-div');
+const permButton = document.querySelector('#perm-button');
+const mainDiv = document.querySelector('#main-div');
 
 browser.storage.local.get('enabled', data => {
     enabled = !!data.enabled;
     if (enabled) {
         enableToggle.setAttribute('checked', enabled);
     }
-    enableToggleLabel.textContent = enabled ? 'Disable' : 'Enable';
+    enableToggleLabel.textContent = enabled ? 'ON' : 'OFF';
 });
 
 enableToggle.addEventListener('change', () => {
@@ -18,6 +21,26 @@ enableToggle.addEventListener('change', () => {
     else {
         enableToggle.removeAttribute('checked');
     }
-    enableToggleLabel.textContent = enabled ? 'Disable' : 'Enable';
+    enableToggleLabel.textContent = enabled ? 'ON' : 'OFF';
     browser.storage.local.set({ enabled: enabled });
+});
+
+permButton.addEventListener('click', (event) => {
+    browser.permissions.request({
+        origins: ['*://*/*']
+    }, (granted) => {
+        if (granted) {
+            browser.storage.local.set({ allSitesAccess: true, enabled: true });
+            buttonDiv.classList.add('off');
+            mainDiv.classList.remove('off');
+        }
+    });
+});
+
+browser.storage.local.get('allSitesAccess', data => {
+    const allSitesAccess = !!data.allSitesAccess;
+    if (allSitesAccess) {
+        buttonDiv.classList.add('off');
+        mainDiv.classList.remove('off');
+    }
 });
